db1fe1f6423c98d2803a4df7b5761f12b9736755
beauspot <beauspot@outlook.com>
2023-11-24 11:30:37 +0100

    altered and fixed validation middleware for the signing up endpoint.

--- Customer/src/api/helpers/middlewares/zod_validation.ts
+++ Customer/src/api/helpers/middlewares/zod_validation.ts
@@ -21,26 +21,31 @@ export const authSchema = z.object({
       required_error: "customer number is required",
       invalid_type_error: "customer number must be a string",
     }),
-    password: z.string({
-      required_error: "password is required",
-      invalid_type_error: "password must be a string",
-    }),
-    confirmPassword: z.string({
-      required_error: "the same password is required to be confirmed",
-      invalid_type_error: "required password must be a string",
-    }),
+    password: z
+      .string({
+        required_error: "password is required",
+        invalid_type_error: "password must be a string",
+      })
+      .min(8),
+    confirmPassword: z
+      .string({
+        required_error: "the same password is required to be confirmed",
+        invalid_type_error: "required password must be a string",
+      })
+      .min(8),
   }),
 });
 
 export const validate =
-  (authSchema: AnyZodObject) =>
-  async (req: Request, res: Response, next: NextFunction) => {
+  (Schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
     try {
-      await authSchema.parseAsync({
+      Schema.parse({
         body: req.body,
         query: req.query,
         params: req.params,
       });
+      next();
     } catch (error: any) {
       return res.status(400).json({ error: error.message });
     }
