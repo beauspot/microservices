57709e6c553d916b48633305394d1bb69ad8701b
beauspot <beauspot@outlook.com>
2023-11-30 21:04:17 +0100

    fixed this testing issue.

--- Customer/src/__tests__/app.test.ts
+++ Customer/src/__tests__/app.test.ts
@@ -1,10 +1,13 @@
 import request from "supertest";
+import AppConfig from "../api/helpers/config/AppConfig";
+
+const API_PREFIX = AppConfig.API_PREFIX;
 
 import app from "../app";
 
 describe("Test the app.ts file", () => {
   test("Testing Index Routes", async () => {
-    const response = await request(app).get("/");
+    const response = await request(app).get(`/${API_PREFIX}`);
     expect(response.body).toEqual({
       message: "Welcome to the User-Micro-Service rest api application.",
     });
