85ee01542dfe0ff644e8fe121af1e63ef6d39a7f
beauspot <beauspot@outlook.com>
2023-11-30 19:58:58 +0100

    changed the expiration of the token and refresh token.

--- Customer/src/api/helpers/utils/refreshToken.ts
+++ Customer/src/api/helpers/utils/refreshToken.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateRefreshToken = (id: string) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET!, { expiresIn: "3d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET!, { expiresIn: "1h" });
 };
