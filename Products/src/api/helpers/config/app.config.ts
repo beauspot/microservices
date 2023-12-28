d885540c46145f59c2aba1362f86ec31813038a1
beauspot <beauspot@outlook.com>
2023-12-28 07:30:10 +0100

    application config file

---
+++ Products/src/api/helpers/config/app.config.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const env = (key: any, defaultValue = null) => {
+  return process.env[key] || defaultValue;
+};
+
+export const applicationConfig = {
+    API_PREFIX: process.env.API_PREFIX,
+    NODE_ENV: process.env.NODE_ENV || "development",
+    PORT: process.env.PORT || 3002,
+    MONGO_URI: process.env.MONGO_URL,
+}
