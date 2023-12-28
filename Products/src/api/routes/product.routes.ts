eb9991e68747d814873328fdbc57f2569208d2e4
beauspot <beauspot@outlook.com>
2023-12-28 08:01:31 +0100

    took out 'products' and changed it to 'prods' in the route setup file.

--- Products/src/api/routes/product.routes.ts
+++ Products/src/api/routes/product.routes.ts
@@ -4,12 +4,12 @@ import { ProductServiceController } from "../controllers/products.controller";
 const router = express.Router();
 
 router
-  .route("/products")
+  .route("/prods")
   .get(ProductServiceController.getAllProducts)
   .post(ProductServiceController.createProductController);
 
 router
-  .route("/products/:id")
+  .route("/prods/:id")
   .get(ProductServiceController.getSingleProduct)
   .patch(ProductServiceController.updateSingleProduct)
   .delete(ProductServiceController.deleteProduct);
