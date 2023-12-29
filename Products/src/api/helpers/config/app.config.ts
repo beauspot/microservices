3fec472fff45b173c7b0140aa38b0e6d64aa0c48
beauspot <beauspot@outlook.com>
2023-12-29 12:20:07 +0100

    changed applicationConfig to AppConfig

--- Products/src/api/helpers/config/app.config.ts
+++ Products/src/api/helpers/config/app.config.ts
@@ -6,7 +6,7 @@ const env = (key: any, defaultValue = null) => {
   return process.env[key] || defaultValue;
 };
 
-export const applicationConfig = {
+export const AppConfig = {
     API_PREFIX: process.env.API_PREFIX,
     NODE_ENV: process.env.NODE_ENV || "development",
     PORT: process.env.PORT || 3002,
